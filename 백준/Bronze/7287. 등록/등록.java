c3d12463c31e2e241a985b2cd7b39ab6b594b6c5
TNALSA <106741517+TNALSA@users.noreply.github.com>
2024-05-23 09:57:19 +0900

    [Bronze V] Title: 등록, Time: 112 ms, Memory: 13816 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+    	int solved = 88;
+    	String myId = "vita";
+    	
+    	System.out.println(solved);
+    	System.out.println(myId);
+    }
+}
