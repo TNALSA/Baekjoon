c6666628b8fbc04dc0f39de5e2ac655e30077139
Retto <106741517+TNALSA@users.noreply.github.com>
2024-08-14 10:44:57 +0900

    [Bronze II] Title: Hashing, Time: 212 ms, Memory: 17720 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -4,16 +4,23 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int L = sc.nextInt();
-        int sum = 0;
-        char[] s;
+        long mod = 1234567891;
+        long hashValue = 0;
 
-        String S =sc.next();
-        s = S.toCharArray();
+        String input = sc.next();
 
-        for(int i=0;i<s.length;i++){
-            sum += (s[i]-96) * (int) Math.pow(31, i);
+        for(int i = 0; i < L; i++){
+            // 알파벳에 대한 매핑 (a: 1, b: 2, c: 3, ...)
+            int charValue = input.charAt(i) - 'a' + 1;
+
+            // 31^i % mod 계산
+            long power = 1;
+            for(int j = 0; j < i; j++){
+                power = (power * 31) % mod;
+            }
+            hashValue = (hashValue + charValue * power) % mod;
         }
-        System.out.println(sum);
+        System.out.println(hashValue);
         }
     }
 
